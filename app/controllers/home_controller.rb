a067f306ced1efbb9e57759db6962f1d466bb148
sajeevan <sajeevanjspy@gmail.com>
2025-06-03 23:09:19 +0530

    feat(controller): add Home dashboard and update navbar with signed-in user profile

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,4 +7,23 @@ class HomeController < ApplicationController
       @login_logs = @user.login_logs.order(logged_in_at: :desc).limit(5)
     end
   end
+
+  def dashboard
+    if user_signed_in?
+      @user = current_user
+      @is_admin = @user.admin_role?
+      @account = @user.accounts.first
+      @transactions = Transaction.where("from_id = ? OR to_id = ?", @account.id, @account.id).order(created_at: :desc)
+    else
+      @user = "anonymous"
+      @is_admin = false
+    end
+  end
+
+  private
+  module AdminActions
+  end
+
+  module CustomerActions
+  end
 end
